9a38531c521b980a6ed8c9e7be8c23a3ab535e8a
Inori_Lover <337938188.ad@gmail.com>
2019-11-11 17:46:47 +0800

    添加babel相关设置；修正ts-loader位置

--- config/babel/index.ts
+++ config/babel/index.ts
@@ -2,8 +2,15 @@ import * as ConfigCreator from 'webpack-chain'
 import blockName from '../block_name'
 
 export const option = {
-  presets: [],
-  plugins: [],
+  presets: [
+    ['@babel/preset-env', { useBuiltIns: 'usage', corejs: 3 }],
+    ['@babel/preset-react'],
+  ],
+  plugins: [
+    ['@babel/plugin-transform-runtime', { corejs: 3 }],
+    ['@babel/plugin-proposal-decorators', { 'legacy': true }],
+    ['@babel/plugin-proposal-class-properties', { 'loose' : true }],
+  ],
 }
 
 export default function (config: ConfigCreator) {
