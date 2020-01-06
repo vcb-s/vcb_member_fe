caac62a313435b9f87f1d958e160fb5f7ba717b4
inori-lover <337938188@qq.com>
2020-01-06 23:08:14 +0800

    添加then-Promise

--- src/utils/sagasCreator.ts
+++ src/utils/sagasCreator.ts
@@ -12,13 +12,27 @@ interface ActionMapBuilder {
   ): ActionMapBuilder;
 }
 
+async function async() {
+  await Promise.resolve()
+}
+
 function sagasCreator (
   func: (builder: ActionMapBuilder) => void
 ) {
-  const sagasMap = new Map()
+  const sagasMap: Map<string, (action: Action) => Promise<void>> = new Map()
   const sagasRuner = (action: Action) => {
     if (sagasMap.has(action.type)) {
-      return sagasMap.get(action.type)(action)
+      /**
+       * @TODO 这里其实不确定dispatch后是否会有异步间隙
+       *
+       * @desc
+       * 根据简单conosle顺序测试，
+       * dispatch后下一行console得到的store是更新后的，
+       * 函数组件也已经跑过一次
+       *
+       * 所以这里的then按推理是可以去掉的，纯粹是一个安心感
+       */
+      return sagasMap.get(action.type)(action).then(async)
     }
 
     throw new Error(`unknown sagas: ${action}`)
