cae00a218c56377ba4fff5b8d395daf3dfc437f0
inori-lover <337938188@qq.com>
2020-01-10 22:51:59 +0800

    简化实现图片懒加载实现

--- src/components/waterfall/item.tsx
+++ src/components/waterfall/item.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState, useEffect } from 'react'
+import React, { FC, useState, useEffect, useCallback } from 'react'
 import { useInView } from 'react-intersection-observer'
 
 import { UserCard } from '~/utils/types'
@@ -12,31 +12,32 @@ interface WaterFallListItemProps {
 }
 type WaterFallListItem = FC<WaterFallListItemProps>
 const WaterFallListItem: WaterFallListItem = ({ data, className, onResize }) => {
-  const [show, setShow] = useState(false)
+  const [avast, setAvast] = useState('')
   const [ref, inView] = useInView()
   const [height, setHeight] = useState('0px')
 
   useEffect(() => {
-    if (inView && !show) {
-      setShow(true)
+    if (inView) {
+      setAvast(data.avast)
     }
   }, [inView])
 
-  const imgLoadedHandle = (e: React.SyntheticEvent<HTMLImageElement, Event>) => {
+  const imgLoadedHandle = useCallback((e: React.SyntheticEvent<HTMLImageElement, Event>) => {
     setHeight('auto')
     onResize && onResize(e)
-  }
-  const errorHandle = () => {
-    const avast = data.avast
+  }, [setHeight])
+
+  const errorHandle = useCallback((e: React.SyntheticEvent<HTMLImageElement, Event>) => {
     const errmsg = avast.replace(/^(.+\/)(.+)(\.)(.+)$/, `图片加载失败：$2 - $4\n$2$3$4`)
     console.warn(errmsg)
-  }
+    onResize && onResize(e)
+  }, [avast])
 
   return (
     <div className={`com_waterfall_item ${className} ${ data.retired ? 'retired' : 'not_retired' }`} ref={ ref }>
       <div className='com_waterfall_item_avast'>
         {/* decoding=async 会触发一次chrome内置的lazy逻辑，导致快速滚动时会出现img的图片请求会出现瞬间被cancel的现象 */}
-        <img src={show ? data.avast : undefined} onLoad={imgLoadedHandle} onError={errorHandle} style={{ height }} />
+        <img src={avast} onLoad={imgLoadedHandle} onError={errorHandle} style={{ height }} />
       </div>
       <div className='com_waterfall_item_text_meta'>
         <div className='com_waterfall_item_text_nickname'>{ data.nickname }</div>
