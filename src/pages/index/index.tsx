22957534287fd392e9e97f559631d2c49255aad5
inori-lover <337938188@qq.com>
2020-01-10 23:07:39 +0800

    添加loading展示

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -3,11 +3,9 @@ import { useSelector } from 'react-redux'
 
 import WaterFall from '~/components/waterfall/async'
 import Footer from '~/components/footer/async'
-// import GroupSwitcher from '~/components/group_switcher/async'
+import Loading from '~/components/loading'
 
 import { slice as globalSlice, State as GlobalState } from '~/models/app'
-
-import loadingIcon from '~/assets/loading.svg'
 
 import './index.scss'
 
@@ -18,8 +16,11 @@ export default React.memo(function IndexPage () {
   return (
     <div className='modules_member_index'>
       <div className='modules_member_index_title'>VCB-Studio 社员一览</div>
+
+      <Loading show={users.loading} />
+
       <WaterFall data={users.data} />
-      <Footer hide={users.loading} />
+      <Footer />
       {/* {group.length ? <GroupSwitcher menuItem={group} onChange={groupChangeHandle} /> : null} */}
     </div>
   )
