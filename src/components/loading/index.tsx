17aa02eaa4b9dd848369b3e89cab6a2d8ba187a4
inori-lover <337938188@qq.com>
2020-01-10 23:07:25 +0800

    实现loading

--- src/components/loading/index.tsx
+++ src/components/loading/index.tsx
@@ -1,5 +1,21 @@
 import React, { FunctionComponent, memo } from 'react'
 
-const Loading: FunctionComponent = () => null
+import loadingIcon from '~/assets/loading.svg'
+import './index.scss'
+
+interface Props {
+  show?: boolean
+}
+const Loading: FunctionComponent<Props> = ({ show = true }) => {
+  if (!show) {
+    return null
+  }
+
+  return (
+    <div className='com_loadingWrap'>
+      <img className='com_loading' src={loadingIcon} alt='' />
+    </div>
+  )
+}
 
 export default memo(Loading)
