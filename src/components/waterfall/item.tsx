50e9e8438bc79179c4cf425a50003bcc22c1b95d
inori-lover <337938188@qq.com>
2020-01-11 23:37:28 +0800

    添加小屏适配

--- src/components/waterfall/item.tsx
+++ src/components/waterfall/item.tsx
@@ -1,7 +1,9 @@
 import React, { FC, useState, useEffect, useCallback } from 'react'
 import { useInView } from 'react-intersection-observer'
+import { useMedia } from 'react-use'
+import classnames from 'classnames'
 
-import { UserCard } from '~/utils/types'
+import { UserCard, GO_BOOL } from '~/utils/types'
 
 import './index.scss'
 
@@ -14,7 +16,7 @@ type WaterFallListItem = FC<WaterFallListItemProps>
 const WaterFallListItem: WaterFallListItem = ({ data, className, onResize }) => {
   const [avast, setAvast] = useState('')
   const [ref, inView] = useInView()
-  const [height, setHeight] = useState('0px')
+  const isWide = useMedia('(min-width: 1080px)')
 
   useEffect(() => {
     if (inView) {
@@ -23,9 +25,8 @@ const WaterFallListItem: WaterFallListItem = ({ data, className, onResize }) => 
   }, [inView])
 
   const imgLoadedHandle = useCallback((e: React.SyntheticEvent<HTMLImageElement, Event>) => {
-    setHeight('auto')
     onResize && onResize(e)
-  }, [setHeight])
+  }, [])
 
   const errorHandle = useCallback((e: React.SyntheticEvent<HTMLImageElement, Event>) => {
     const errmsg = avast.replace(/^(.+\/)(.+)(\.)(.+)$/, `图片加载失败：$2 - $4\n$2$3$4`)
@@ -34,10 +35,20 @@ const WaterFallListItem: WaterFallListItem = ({ data, className, onResize }) => 
   }, [avast])
 
   return (
-    <div className={`com_waterfall_item ${className} ${ data.retired ? 'retired' : 'not_retired' }`} ref={ ref }>
+    <div
+      className={
+        classnames({
+          'com_waterfall_item': true,
+          [className]: !!className,
+          'retired': data.retired === GO_BOOL.yes,
+          'small': !isWide,
+        })
+      }
+      ref={ ref }
+    >
       <div className='com_waterfall_item_avast'>
         {/* decoding=async 会触发一次chrome内置的lazy逻辑，导致快速滚动时会出现img的图片请求会出现瞬间被cancel的现象 */}
-        <img src={avast} onLoad={imgLoadedHandle} onError={errorHandle} style={{ height }} />
+        <img src={avast} onLoad={imgLoadedHandle} onError={errorHandle} />
       </div>
       <div className='com_waterfall_item_text_meta'>
         <div className='com_waterfall_item_text_nickname'>{ data.nickname }</div>
