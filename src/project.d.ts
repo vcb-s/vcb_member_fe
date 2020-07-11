dfe6387a4f443d071f9dbd3d55bb2abd71f7bbde
inori-lover <337938188@qq.com>
2020-07-11 20:08:05 +0800

    feat: 尽量逼近最合适的module描述

--- src/project.d.ts
+++ src/project.d.ts
@@ -2,7 +2,15 @@ declare module 'fibers';
 declare module 'masonry-layout';
 declare module 'intersection-observer';
 
-declare module '*.scss';
+declare module '*.scss' {
+  interface T_CSSModule {
+    [key: string]: string;
+  }
+
+  const CSSModule: T_CSSModule;
+
+  export default CSSModule;
+}
 declare module '*.svg';
 declare module '*.png';
 declare module '*.jpg';
