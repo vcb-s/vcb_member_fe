337a9c5b432a313188f59febe638a4158f92ca57
inori_lover <inori_lover@outlook.com>
2022-05-22 16:42:28 +0800

    chore: 升级依赖，简化配置，移除cdn

--- .umirc.ts
+++ .umirc.ts
@@ -1,93 +1,64 @@
-import { defineConfig } from 'umi';
-// import { writeFileSync } from 'fs';
+import { defineConfig } from "umi";
 
-const { PROXY_TARGET = 'https://vcb-s.com' } = process.env;
+const { PROXY_TARGET = "https://vcb-s.com" } = process.env;
 
-const __DEV__ = process.env.NODE_ENV === 'development';
-
-const cacheHost = '';
-const base = '/vcbs_member/';
-const publicPath = __DEV__ ? base : `${cacheHost}${base}`;
+const __DEV__ = process.env.NODE_ENV === "development";
 
 export default defineConfig({
-  title: 'vcb-s成员介绍',
-  base: '/vcbs_member/',
-  publicPath: publicPath,
+  title: "vcb-s成员介绍",
+  base: "/vcbs_member/",
 
   hash: true,
   dynamicImport: {},
-  // forkTSChecker: {},
-  nodeModulesTransform: { type: 'none' },
-  favicon: '/assets/favicon.ico',
+  favicon: "/assets/favicon.ico",
 
   analyze: {
-    analyzerMode: 'static',
+    analyzerMode: "static",
     openAnalyzer: true,
   },
 
   proxy: {
-    '/vcbs_member_api': {
+    "/vcbs_member_api": {
       target: PROXY_TARGET,
       changeOrigin: true,
     },
   },
 
-  externals: {
-    react: 'window.React',
-    'react-dom': 'window.ReactDOM',
-    axios: 'window.axios',
-    'masonry-layout': 'window.Masonry',
-  },
+  headScripts: __DEV__
+    ? []
+    : [
+        // flags=always
+        "https://polyfill.alicdn.com/polyfill.min.js?flags=gated&features=AbortController%2CURL%2CURLSearchParams%2Cfetch%2CMap%2CWeakMap%2CSet%2CWeakSet",
+      ],
 
-  headScripts: [
-    // flags=always
-    'https://polyfill.alicdn.com/polyfill.min.js?flags=gated&features=URL%2CURLSearchParams%2Cfetch%2CMap%2CWeakMap%2CSet%2CWeakSet',
-  ],
-
-  scripts: [
-    'https://cdn.jsdelivr.net/npm/masonry-layout@4.2.2/dist/masonry.pkgd.min.js',
-    'https://cdn.jsdelivr.net/npm/axios@0.21.1/dist/axios.min.js',
-
-    'https://cdn.jsdelivr.net/npm/react@17.0.1/umd/react.production.min.js',
-    'https://cdn.jsdelivr.net/npm/react-dom@17.0.1/umd/react-dom.production.min.js',
-  ],
-
-  links: [
-    {
-      rel: 'dns-prefetch',
-      href: 'https://cdn.jsdelivr.net',
-    },
-  ],
-
-  sass: {
-    sassOptions: {
+  sass: {},
+  terserOptions: {
+    compress: {
+      drop_console: true,
     },
   },
+
+  /** @link https://umijs.org/config#targets */
+  targets: __DEV__
+    ? {
+        chrome: 100,
+        firefox: false,
+        safari: false,
+        edge: false,
+        ios: false,
+      }
+    : {
+        /** @link https://caniuse.com/es6-module-dynamic-import */
+        chrome: 63,
+        firefox: 67,
+        safari: 11.1,
+        edge: 79,
+        ios: 11,
+      },
+
+  webpack5: {},
 
   chainWebpack: (config) => {
-    config.module
-      .rule('js')
-      .use('babel-loader')
-      .tap((options) => {
-        options.presets[0][1].env = {
-          ...options.presets[0][1].env,
-          exclude: [
-            ...(options.presets[0][1].env.exclude || []),
-            /web\.url/,
-            'es.promise',
-            'es.weak-set',
-            'es.weak-map',
-            'es.map',
-            'es.set',
-          ],
-        };
-        // console.log('===');
-        // console.log(options.presets);
-        // console.log('===');
-        return options;
-      });
-    // writeFileSync('./config.js', config.toString());
-    // process.exit(0);
     return config;
   },
 });
